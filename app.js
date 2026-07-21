fc5277546665f18ed9c3942a50f5a0f4febc4b04
Lkop172 <lukiejohnson9@gmail.com>
2026-07-21 11:24:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-console.log("NEW APP JS IS RUNNING");
-
-console.log(tournament);
-
-console.log("First team:");
-console.log(tournament.teams[0]);
+console.log("Tournament loaded");
 
 const standings = document.getElementById("teamStandings");
 
-console.log("Standings location:");
-console.log(standings);
+
+// Calculate team totals
+
+let rankedTeams = tournament.teams.map(function(team) {
+
+    let scores = team.scores.map(function(player) {
+        return player.pins;
+    });
+
+    scores.sort(function(a, b) {
+        return b - a;
+    });
+
+    let total = scores.slice(0,4).reduce(function(sum, score) {
+        return sum + score;
+    },0);
+
+    return {
+        name: team.name,
+        pins: total
+    };
+
+});
+
+
+// Sort highest score first
+
+rankedTeams.sort(function(a,b){
+    return b.pins - a.pins;
+});
+
+
+// Display standings
+
+rankedTeams.forEach(function(team,index){
+
+    let row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${team.name}</td>
+        <td>${team.pins}</td>
+    `;
+
+    standings.appendChild(row);
+
+});

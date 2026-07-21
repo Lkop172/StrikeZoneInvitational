82c9299888782d60454299b3e6fb48f7a856f19d
Lkop172 <lukiejohnson9@gmail.com>
2026-07-21 11:23:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-console.log("App loaded");
+console.log("NEW APP JS IS RUNNING");
+
+console.log(tournament);
+
+console.log("First team:");
+console.log(tournament.teams[0]);
 
 const standings = document.getElementById("teamStandings");
 
-tournament.teams.forEach(function(team, index) {
-
-    // Get all bowler scores
-    let scores = team.scores.map(function(player) {
-        return player.pins;
-    });
-
-    // Sort highest to lowest
-    scores.sort(function(a, b) {
-        return b - a;
-    });
-
-    // Drop the lowest score
-    let countedScores = scores.slice(0, 4);
-
-    // Add the top four scores
-    let teamTotal = countedScores.reduce(function(total, score) {
-        return total + score;
-    }, 0);
-
-
-    let row = document.createElement("tr");
-
-    row.innerHTML = `
-        <td>${index + 1}</td>
-        <td>${team.name}</td>
-        <td>${teamTotal}</td>
-    `;
-
-    standings.appendChild(row);
-
-});
+console.log("Standings location:");
+console.log(standings);

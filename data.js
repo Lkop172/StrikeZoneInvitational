7a1e9e9b3b9e6cdff5a4073484026de17e78a21d
Lkop172 <lukiejohnson9@gmail.com>
2026-07-21 10:56:33 -0500

    Added tournament data

---
+++ data.js
@@ -0,0 +1,63 @@
+const tournament = {
+
+    name: "Strike Zone Invitational 2026",
+
+    currentRound: "Prelims",
+
+    teams: [
+
+        {
+            name: "Team Apex",
+            seed: null,
+            bowlers: [
+                "Jake Mitchell",
+                "Ryan Carter",
+                "Mason Brooks",
+                "Tyler Reed",
+                "Noah Bennett"
+            ],
+            scores: []
+        },
+
+        {
+            name: "Team Inferno",
+            seed: null,
+            bowlers: [
+                "Ethan Parker",
+                "Lucas Hayes",
+                "Caleb Turner",
+                "Owen Foster",
+                "Dylan Wright"
+            ],
+            scores: []
+        },
+
+        {
+            name: "Team Velocity",
+            seed: null,
+            bowlers: [
+                "Aiden Collins",
+                "Logan Pierce",
+                "Carter Hayes",
+                "Blake Morgan",
+                "Jackson King"
+            ],
+            scores: []
+        },
+
+        {
+            name: "Team Wave",
+            seed: null,
+            bowlers: [
+                "Liam Scott",
+                "Connor Davis",
+                "Hunter Wilson",
+                "Mason Clark",
+                "Evan Brooks"
+            ],
+            scores: []
+        }
+
+    ]
+
+};
